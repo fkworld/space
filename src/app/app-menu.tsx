297481556df2d8a8b3eeeccd5863ca9cc8b5aa40
fkworld <fkworld@foxmail.com>
2022-11-03 11:44:14 +0800

    chore: switch to lodash-es

--- src/app/app-menu.tsx
+++ src/app/app-menu.tsx
@@ -1,29 +1,19 @@
-import { GithubOutlined } from '@ant-design/icons'
 import { Menu, MenuProps } from 'antd'
-import { uniqueId } from 'lodash'
 import React, { FC } from 'react'
 import { Link } from 'react-router-dom'
 
 export const AppMenu: FC = () => {
   const items: MenuProps['items'] = [
     {
-      key: uniqueId(),
+      key: 'home',
       label: <Link to="/">首页</Link>,
     },
     {
-      key: uniqueId(),
-      label: '菜单 1',
-    },
-    {
-      key: uniqueId(),
-      label: '菜单 2',
-    },
-    {
-      key: uniqueId(),
+      key: 'special',
       label: '特殊页面',
       children: [
-        { key: uniqueId(), label: <Link to="/403">403</Link> },
-        { key: uniqueId(), label: <Link to="/404">403</Link> },
+        { key: 'special-403', label: <Link to="/403">403</Link> },
+        { key: 'special-404', label: <Link to="/404">403</Link> },
       ],
     },
   ]
